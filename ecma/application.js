ddb09a716484b90f4d4216f09364d3414d078114
Aleks <aleks@mac>
2019-04-18 16:47:48 +0300

    Application js added
    * Temp css removed
    * Temp slim layouts removed
    * IE hack removed

---
+++ ecma/application.js
@@ -0,0 +1,99 @@
+/**
+ * globals
+ */
+var MOBILE_VIEW = 992;
+
+$(function() {
+  'use strict';
+
+  function getWidth() {
+    return window.innerWidth;
+  }
+
+  var App = {
+      
+      
+      /**
+       * init
+       */
+      init: function() {
+        this.cacheElements();
+        this.bindEvents();
+        this.checkViewport();
+        
+        $(function () {
+          $('[data-toggle="tooltip"]').tooltip()
+        })
+        $("select.huge").selectpicker({style: 'btn-sm', menuStyle: ''});
+        $("select[name='large']").selectpicker({style: 'btn-lg btn-danger'});
+        $("select[name='info']").selectpicker({style: 'btn-info'});
+        $("select[name='small']").selectpicker({style: 'btn-sm btn-warning'});
+        
+        $('.list-row-action-delete-one').on('click', function(ev) {
+          ev.preventDefault();
+          $(this).parent().next().modal();
+        });
+
+      },
+
+      /**
+       * typeahead
+       */
+      
+      /**
+       * cache elements
+       */
+      cacheElements: function() {
+        this.$viewport    = $(window);
+        this.$pageWrapper = $("#page-wrapper");
+        this.$toggleBtn   = $("#toggle-sidebar");
+      },
+
+      /**
+       * bind events to elements
+       */
+      bindEvents: function() {
+        this.$viewport.on('resize', this.viewportResize.bind(this));
+        this.$toggleBtn.on('click', this.toggleSidebar.bind(this));
+      },
+
+      /**
+       * trigger checkviewport on resize
+       */
+      viewportResize: function() {
+        this.checkViewport();
+      },
+
+      /**
+       * toggles sidebar
+       */
+      toggleSidebar: function(e) {
+        this.$pageWrapper.toggleClass('open');
+
+        $.cookie('toggle', this.$pageWrapper.hasClass("open"), { path: '/' });
+      },
+
+      /**
+       * Checks the viewport and toggles sidebar if toggled
+       */
+      checkViewport: function() {
+        if (getWidth() >= MOBILE_VIEW) {
+          if ($.cookie('toggle') === undefined) {
+               this.$pageWrapper.addClass("open");
+           } else {
+               if($.cookie('toggle') == 'true') {
+                   this.$pageWrapper.addClass("open");
+               } else {
+                   this.$pageWrapper.removeClass("open");
+               }
+           }
+        } else {
+            this.$pageWrapper.removeClass("open");
+        }
+      },
+
+  };
+
+  App.init();
+
+});
