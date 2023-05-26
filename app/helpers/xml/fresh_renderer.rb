cf0ddeef40e0bf0416a80e681b252962edfafa5b
ITilyaGO <42610063+ITilyaGO@users.noreply.github.com>
2023-05-26 16:12:36 +0300

    Added export products 1c page

--- app/helpers/xml/fresh_renderer.rb
+++ app/helpers/xml/fresh_renderer.rb
@@ -130,4 +130,79 @@ module Xmlfr
     Ox.dump doc
   end
 
+def customer_from_products_list(ps)
+    n = wonderbox(:w1c, :number).succ
+    tnow = Time.now.strftime("%FT%T")
+
+    doc = create_doc_xml('V8Exch:_1CV8DtUD')
+    doc.root << (ex = create_plain_node('V8Exch:Data'))
+
+    ex << epro = Ox.load(TML_CAT)
+    replace_node epro, 'Description', 'Pio'
+    replace_node epro, 'ДатаИзменения', Datezero
+    replace_node epro, 'Pio', Uidone
+    replace_node epro, 'ParentPio', Uidzero
+
+    codes = ps.map(&:place_id).uniq
+    kc_towns = KatoAPI.batch(codes)
+    cats = ps.map{|p|[p.category_id, p.category.category_id]}.flatten.uniq
+    cs = KSM::Category.find_all(cats)
+    sects = cs.select(&:top?).map(&:section).uniq(&:id)
+    sects.each do |sec|
+      ex << epro = Ox.load(TML_CAT)
+      replace_node epro, 'Description', sec.name
+      replace_node epro, 'ДатаИзменения', Datezero
+      replace_node epro, 'Pio', format_cat_1c(sec.id)
+      replace_node epro, 'ParentPio', Uidone
+    end
+    cs.sort_by{|c|c.top? ? 0 : 1}.each do |cat|
+      ex << epro = Ox.load(TML_CAT)
+      replace_node epro, 'Description', cat.name
+      replace_node epro, 'ДатаИзменения', Datezero
+      replace_node epro, 'Pio', format_cat_1c(cat.id)
+      replace_node epro, 'ParentPio', format_cat_1c(cat.top? ? cat.section_id : cat.category_id)
+    end
+    ps.each do |product|
+      ex << epro = Ox.load(TML_PRODUCT)
+      replace_node epro, 'НаименованиеПолное', product.displayname(text: true)
+      replace_node epro, 'Description', product.displayname(text: true)
+      replace_node epro, 'ДатаИзменения', Datezero
+      replace_node epro, 'Артикул', product.art.to_s
+      replace_node epro, 'Pio', format_product_1c(product.id)
+      replace_node epro, 'ParentPio', format_cat_1c(product.category_id || '0000')
+
+      replace_node epro, 'Вес', (product.dim_weight || 0).to_s
+      replace_node epro, 'Ширина', (product.dim_width || 0).to_s
+      replace_node epro, 'Высота', (product.dim_height || 0).to_s
+      replace_node epro, 'Длина', (product.dim_length || 0).to_s
+
+      replace_node epro, 'РасшЯрд_Город', kc_towns[product.place_id]&.model.name || ''
+      replace_node epro, 'Комментарий', product.desc if product.desc
+    end
+
+    ex << edict = Ox.load(TML_INVOICE)
+    replace_node edict, 'Date', tnow
+    replace_node edict, 'Number', format_num_1c(n)
+    replace_node edict, 'Комментарий', ''
+    replace_node edict, 'СуммаДокумента', '0'
+
+    scs = KSM::Section.all
+    ps.each do |product|
+      a = 1
+      item_sum = (product.price.to_i || 0).round(2)
+      edict << row = Ox.load(TML_INVOICE_POS)
+
+      replace_node row, 'Pio', format_product_1c(product.id)
+      replace_node row, 'Цена', product.price.to_s
+      replace_node row, 'Сумма', item_sum.to_s
+      replace_node row, 'Всего', item_sum.to_s
+      replace_node row, 'Количество', a.to_s
+    end
+    doc.root << create_plain_node('PredefinedData')
+
+    wonderbox_set(:w1c, { number: n })
+
+    Ox.dump doc
+  end
+
 end
